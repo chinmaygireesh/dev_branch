bc092d5f118be16ed9fd5c911524db79115ec69b
chinmaygireesh <chinmaygireeshs@gmail.com>
2021-07-08 16:09:13 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arr = new int[]  { 3,3,3};
+            int i,j;
+            bool flag;
+            for(i=0;i<arr.Length;i++){
+                
+                flag = true;//assuming as a leader
+                for(j=i+1;j<arr.Length;j++){
+                    if(arr[j]>arr[i]){
+                        flag = false; //not a leader
+                        break;
+                    }
+                    }
+                if(flag){//still if it is leader
+                    //Console.WriteLine("yes");
+                    Console.Write($"{arr[i]} ");
+                }    
+                
+            }
+         //   Console.WriteLine("Hello World chinmay!");
+        }
+    }
+}
